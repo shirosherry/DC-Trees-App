0f6173b4fe7d81885dd8cfac389c53b511401e93
sherrisherry <34172217+sherrisherry@users.noreply.github.com>
2019-03-28 16:18:40 -0400

    pkg

--- pkg/R/gfi_functions.R
+++ pkg/R/gfi_functions.R
@@ -1,15 +1,42 @@
 bridge_cols <- function(nm, cl){
   cols <- rep('NULL',21)
   names(cols) <- c('un_code','imf_code','imf_nm','un_nm','d_gfi','d_dev','d_ssa','d_asia','d_deur','d_mena','d_whem','d_adv','un_nm_en_full','un_nm_en_abbr','un_note','iso2','iso3','un_start','un_end','wb_code','wb_nm')
-  cols[match(nm, names(cols))] <- cl
+  cols[nm] <- cl
   return(cols)
 }
 
+hkrx_cols <- function(nm, cl){
+  cols <- rep('NULL',9)
+  names(cols) <- c("t","origin_hk","consig_hk","k","vrx_hkd","origin_un","consig_un","vrx_un")
+  if(missing(cl))
+    cl <- c(rep("integer",3),"character","numeric",rep("integer",2),"numeric")[match(nm, names(cols))]
+    # c() matches names(cols)
+  cols[nm] <- cl
+  return(cols)
+}
+
+in_bridge <- function(nm, cl, logf, max_try = 10){
+  cols <- bridge_cols(nm, cl)
+  scripting::ecycle(bridge <- s3read_using(FUN = function(x)read.csv(x, colClasses=cols, header=T, fileEncoding = 'UTF8'),
+                                           object = 'bridge.csv', bucket = 'gfi-supplemental'),
+                    {if(!missing(logf))logf(paste('0000', '!', 'loading bridge.csv failed', sep = '\t')); return(NULL)}, max_try)
+  return(bridge)
+}
+
+in_hkrx <- function(yr, nm, cl, logf, max_try = 10){
+  cols <- hkrx_cols(nm, cl)
+  tmp <- tempfile()
+  scripting::ecycle(save_object(object = paste('HK', yr, 'rx.csv.bz2', sep = '_'), bucket = 'gfi-supplemental', file = tmp, overwrite = TRUE),
+                    {if(!missing(logf))logf(paste(yr, '!', 'retrieving hkrx file failed', sep = '\t')); return(NULL)}, max_try) 
+  scripting::ecycle(hk <- read.csv(pipe(paste("bzip2 -dc ", tmp, sep = '')), header=T, colClasses=cols, na.strings="", stringsAsFactors = F),
+                    {if(!missing(logf))logf(paste(yr, '!', 'loading hkrx file failed', sep = '\t')); return(NULL)}, 
+                    max_try, cond = is.data.frame(hk) && nrow(hk)>10)
+  return(hk)
+}
+
 gfi_cty <- function(logf, max_try = 10){
-  cols_bridge <- bridge_cols(c('un_code','d_gfi'), rep('integer',2))
-  ecycle(bridge <- s3read_using(FUN = function(x)read.csv(x, colClasses=cols_bridge, header=TRUE),
-                                object = 'bridge.csv', bucket = 'gfi-supplemental'),
-         {if(!missing(logf))logf(paste('0000', '!', 'loading bridge.csv failed', sep = '\t')); stop('loading bridge.csv failed')}, max_try)
+  bridge <- in_bridge(c('un_code','d_gfi'), rep('integer',2), logf, max_try)
+  if(is.null(bridge))return(NULL)
   bridge <- unique(bridge)
   cty <- bridge$un_code[bridge$d_gfi==1]
   if(!missing(logf))logf(paste('0000', ':', 'decided cty', sep = '\t'))
--- pkg/R/un_comtrade.R
+++ pkg/R/un_comtrade.R
@@ -1,46 +1,72 @@
 #  procedure to adjust treated UN-Comtrade data for Swiss trade in non-monetary gold [710812] prior to 2012
-swiss <- function(df_in,df_swiss,t_in) {
-  # colnames(df_IN) is given by c("hs",i","j","k","v_?","q_code_?","q_?","q_kg_?") (where ?={M,X,rX,rM})
-  # colnames(df_swiss) is given by c("i","j","k","t","v_?","q_kg_?")
+unct_swiss <- function(unct,df_swiss,t_in){
+  # colnames(unct) is given by c("hs","i","j","k","v_?","q_code_?","q_?","q_kg_?") (where ?={M,X,rX,rM})
+  # colnames(df_swiss) is given by c("mx","j","k","t","v","q_kg")
   # program replaces/appends Swiss data to UN-Comtrade dataset, excluding OECD balance adjustments for Germany (2005-2008), and UK (2007-2014)
   #      those might be introduced at country level study when time series are available (relative sharing is required per OECD)
-  df_out <- df_in
-  col_names_in <- colnames(df_in)
-  col_names_swiss_in <- colnames(df_swiss)
-  col_names_temp <- colnames(df_swiss) <- c("i","j","k","t","v","q_kg")
-  col_names_out <- colnames(df_out)
+  cols_out <- colnames(unct); names(cols_out) <- sub('_[^_]+$', '', cols_out)
   if ((t_in < 2012) & (t_in > 1987)) {
     hs <- NA
     # assume Swiss data are harmonized the same way as WITS data
     if (t_in %in% 2000:2001) {hs <- 1}
     if (t_in %in% 2002:2006) {hs <- 2}
     if (t_in %in% 2007:2011) {hs <- 3}
-    temp <- subset(df_swiss,df_swiss$t==t_in)
-    n_temp <- dim(temp)[1]
-    df_hs <- data.frame(hs=rep(hs,n_temp))
-    temp <- cbind(df_hs,temp)
-    swiss_out <- data.frame( hs = rep(hs,n_temp),
-                             i = rep(757,n_temp),
-                             j=rep(0,n_temp),
-                             k=rep("710812",n_temp),
-                             v=rep(0,n_temp),
-                             q_code = rep(8,n_temp), # output quantities in kg
-                             q = rep(1,n_temp),      # q=1
-                             q_kg = rep(0,n_temp))
-    swiss_out$j    <- temp$j
-    swiss_out$v    <- temp$v
-    swiss_out$q_kg <- temp$q_kg
-    colnames(swiss_out) <- col_names_out  # also equal to col_names_in
-    df_out <- subset(df_out,!((df_out$i=757)&(df_out$k="710812")))  # eliminate any existing rows on Swiss reported trade in 710812
-    df_out <- rbind(df_out,swiss_out)
+    df_swiss <- subset(df_swiss,df_swiss$t==t_in)
+    n_df_swiss <- nrow(df_swiss)
+    swiss_out <- data.frame( hs = rep(hs,n_df_swiss),
+                             i = rep(757,n_df_swiss),
+                             j = df_swiss$j,
+                             k=rep("710812",n_df_swiss),
+                             v = df_swiss$v,
+                             q_code = rep(8,n_df_swiss), # output quantities in kg
+                             q = df_swiss$q_kg,      # q=q_kg
+                             q_kg = df_swiss$q_kg )
+    colnames(swiss_out) <- cols_out[colnames(swiss_out)]
+    unct <- subset(unct,!((unct$i=757)&(unct$k="710812")))  # eliminate any existing rows on Swiss reported trade in 710812
+    unct <- rbind(unct,swiss_out)
+    if(data.table::is.data.table(unct))setkeyv(unct, c('i', 'j', 'k'))
   }
-  return(df_out)
+  return(unct)
 }
 
+unct_hk <- function(unct, hk, yr, mx, logf, max_try = 10){
+  ij <- switch(mx, m = c('j', 'i'), x = c('i', 'j'))
+  cols_hk <- c(ij,"k","v_rx_hk")
+  names(cols_hk) <- c("origin_un","consig_un","k","vrx_un")
+  cols_out <- c("hs_rpt","hs_ptn",ij,"k","v_X","v_M","v_rM","v_rX","q_X","q_M","q_kg_X","q_kg_M","q_code_X","q_code_M")
+  if(!setequal(names(cols_hk), colnames(hk)) || !all(cols_out %in% colnames(unct))){
+    if(!missing(logf))logf(paste(yr, '!', 'HK adj col mismatch', sep = '\t'))
+    return(NULL)
+  }
+  colnames(hk) <- cols_hk[colnames(hk)]
+  if(data.table::is.data.table(unct))hk <- data.table::data.table(hk, key = c(ij, 'k'))
+  unct <- merge(x=unct,y=hk,by=c(ij,"k"),all.x=TRUE)
+  hk_344 <- hk[,c(ij[2],"k","v_rx_hk")]
+  colnames(hk_344) <- c(ij[2],"k","adj_344")
+  hk_344 <- aggregate(hk_344$adj_344,list(hk_344[, ij[2]],hk_344$k),sum) ; colnames(hk_344) <- c(ij[2],"k","adj_344")
+  if(data.table::is.data.table(unct))hk_344 <- data.table::data.table(hk_344, key = c(ij[2],'k'))
+  unct <- merge(x=unct,y=hk_344,by=c(ij[2],"k"),all.x=TRUE)
+  unct[is.na(unct$v_rx_hk),"v_rx_hk"] <- 0
+  unct[is.na(unct$adj_344),"adj_344"] <- 0
+  tmp <- unct
+  unct[unct[, ij[1]]!=344,"v_M"] <- unct[unct[, ij[1]]!=344,"v_M"] - unct[unct[, ij[1]]!=344,"v_rx_hk"]
+  unct[unct[, ij[1]]==344,"v_M"] <- unct[unct[, ij[1]]==344,"v_M"] + unct[unct[, ij[1]]==344,"adj_344"]
+  unct <- subset(unct, select = cols_out)
+  junk <- subset(unct,unct$v_M<0)
+  unct[unct$v_M<0,"v_M"] <- tmp[unct$v_M<0,"v_M"]  ; # undo the adjustment for negative values
+  unct[unct[, ij[2]]==752,"v_M"] <- tmp[unct[, ij[2]]==752,"v_M"]  ; # undo the adjustment for Sweden  (OECD[2016], p. 19)
+  unct[unct[, ij[2]]==348,"v_M"] <- tmp[unct[, ij[2]]==348,"v_M"]  ; # undo the adjustment for Hungary (OECD[2016], p. 19)
+  if(!missing(logf))logf(paste(yr, ':', paste(mx, 'HK adjusted'), sep = '\t'))
+  scripting::ecycle(aws.s3::s3write_using(junk, FUN = function(x, y)write.csv(x, file=bzfile(y), row.names = FALSE),
+                                          bucket = out_bucket,
+                                          object = paste('Comtrade', yr, mx, 'junked-HK-adj.csv.bz2', sep = '-')),
+                    if(!missing(logf))logf(paste(yr, '.', paste('junked', mx, 'HK-adj not uploaded'), sep = '\t')), 
+                    ceiling(max_try/3))
+  return(unct)
+}
 #  procedure to treat UN-Comtrade data for know country and commodity quirks (as noted)
-treat <- function(df_in,t_in) {
+unct_treat <- function(unct,t_in) {
   # Eliminate redundant country categories
-  df_out <- df_in
   x_cty <- c(0,     # World
              250,   # France excluding Monaco & overseas (UN data for France includes Monaco and overseas areas)
              1251,  # France including Monaco excluding overseas (UN data for France includes Monaco and overseas areas)
@@ -77,24 +103,24 @@ treat <- function(df_in,t_in) {
              879    #         Western Asia, nes
   )
 
-  df_out <- subset(df_out,!(df_out$i %in% x_cty) & !(df_out$j %in% x_cty))
+  unct <- subset(unct,!(unct$i %in% x_cty) & !(unct$j %in% x_cty))
 
   # Special country treatments
   # (1) Beginning in 2000, exclude San Marino[674] & Vatican[381] (already included in Italy[381])
   if (t_in >= 2000) {
     x_cty <- c(674,381)
-    df_out <- subset(df_out,!(df_out$i %in% x_cty) & !(df_out$j %in% x_cty))
+    unct <- subset(unct,!(unct$i %in% x_cty) & !(unct$j %in% x_cty))
   }
   # (2) Beginning in 2006, adjust Slovakia[703] reports to reflect Montenegro[499] independently from Serbia-Montegro[891]
   if (t_in >= 2006) {
-    # unpack df_out
-    colnames_out <- colnames(df_out)
-    s_891  <-  subset(df_out, df_out$i==703 & df_out$j==891)  # isolate portion of data for which Slovakia reports trade flows with Serbia-Montenegro
+    # unpack unct
+    colnames_out <- colnames(unct)
+    s_891  <-  subset(unct, unct$i==703 & unct$j==891)  # isolate portion of data for which Slovakia reports trade flows with Serbia-Montenegro
     n_891  <- dim(s_891)[1]
     if (n_891 > 0) {
-      s_688 <-   subset(df_out, df_out$i==703 & df_out$j==688)  #  subset of Slovakia reported trade with partner Serbia alone
+      s_688 <-   subset(unct, unct$i==703 & unct$j==688)  #  subset of Slovakia reported trade with partner Serbia alone
       temp <- merge(x=s_891,y=s_688,by=c("hs","i","k"),all.x=TRUE)
-      # colnames(df_IN) is given by c("hs","i","j","k","v_?","q_code_?","q_?","q_kg_?") (where ?={M,X,rX,rM}) so with the left merge
+      # colnames(unct) is given by c("hs","i","j","k","v_?","q_code_?","q_?","q_kg_?") (where ?={M,X,rX,rM}) so with the left merge
       # colnames(temp) will have column structure:
       #    1 "hs"
       #    2 "i"
@@ -119,12 +145,12 @@ treat <- function(df_in,t_in) {
         s_891[,7] <- NA                  # missing
         s_891[,8] <- NA                  # missing
       }
-      df_out <- subset(df_out,!(df_out$i==703 & df_out$j==891))
-      df_out <- rbind(df_out,s_891)
+      unct <- subset(unct,!(unct$i==703 & unct$j==891))
+      unct <- rbind(unct,s_891)
     } # end if (n_891>0)
   } # end if (t>=2006) ...Slovakia treatment
   # NEC commodity exclusions
-  df_out <- df_out[df_out$k!=999999 & df_out$k!="9999AA",]
-  #
-  return(df_out)
+  unct <- unct[unct$k!='999999' & unct$k!="9999AA",]
+  if(data.table::is.data.table(unct))setkeyv(unct, c('i', 'j', 'k'))
+  return(unct)
 }
